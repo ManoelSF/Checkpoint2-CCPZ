95bfea0c991e49d9e781c08d52a3033f7d620b93
andrei-hs <123700875+andrei-hs@users.noreply.github.com>
2026-04-30 14:40:22 -0300

    Add: Segunda parte Ex2

--- Ex5.py
+++ Ex5.py
@@ -35,3 +35,32 @@ emprestimo = float(input("Digite o valor do empréstimo: "))
 while emprestimo <= 0:
     print("O valor do empréstimo deve ser maior que zero.")
     emprestimo = float(input("Digite o valor novamente: "))
+
+print("\nTaxa de Juros")
+print("---------------------")
+print("|Parcelas|   Juros  |")
+print("| 3 - 6  | 5% ao mês|")
+print("| 7 - 12 | 8% ao mês|")
+print("| 13 - 24|10% ao mês|")
+print("---------------------")
+
+parcelas = int(input("Digite o número de parcelas (3-24): "))
+while parcelas < 3 or parcelas > 24:
+    print(f"Valor {parcelas} inválido!")
+    parcelas = int(input("Digite o número de parcelas entre 3 e 24: "))
+
+
+if aprovar_emp(idade, emprestimo, renda_mensal):
+    juros = definir_taxa(parcelas)
+    total_pagar = calcular_total(parcelas, emprestimo, juros)
+
+    print(f"\n✅ Empréstimo APROVADO para {nome}!")
+    print(f"Taxa aplicada: {juros * 100:.0f}% ao mês")
+    print(f"Valor total com juros: R$ {total_pagar:.2f}")
+    print(f"Valor de cada parcela ({parcelas}x): R$ {total_pagar / parcelas:.2f}")
+else:
+    print(f"\n Empréstimo NEGADO para {nome}.")
+    if idade < 18:
+        print("Motivo: Menor de idade.")
+    else:
+        print("Motivo: Valor solicitado excede o limite de 20x a renda mensal.")
